2f4ef775133a3e71376e826cbf8c65a19be8fd21
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-28 03:33:58 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item, include: :users
+  end
+  def index
+    item = Item.all
+    render json: item, include: :user
+  end
 end
